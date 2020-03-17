d2f897ed05e2d197ec14457632b6a18b1fdf0cf9
Couapy <couapy.web@gmail.com>
2020-03-17 17:44:18 +0100

    Fix views for unconnected users

--- blog/static/scripts/comment_app.js
+++ blog/static/scripts/comment_app.js
@@ -1,18 +1,35 @@
 import { Editor, reply, like } from './modules/comment.js'
 
-/**
- * Add an Editor's instance to the beginning of the comments
- */
-var publish_editor = new Editor(0, false)
-document.querySelector('.comments').prepend(publish_editor.toDOM)
-publish_editor.setEvents()
+if (user.connected) {
+    /**
+     * Add an Editor's instance to the beginning of the comments
+     */
+    var publish_editor = new Editor(0, false)
+    document.querySelector('.comments').prepend(publish_editor.toDOM)
+    publish_editor.setEvents()
 
-/**
- * Add events for Reply and Like comments
- */
-document.querySelectorAll('.comment .like').forEach(link => {
-    link.addEventListener('click', like)
-})
-document.querySelectorAll('.comment .reply').forEach(link => {
-    link.addEventListener('click', reply)
-})
+    /**
+     * Add events for Reply and Like comments
+     */
+    document.querySelectorAll('.comment .like').forEach(link => {
+        link.addEventListener('click', like)
+    })
+    document.querySelectorAll('.comment .reply').forEach(link => {
+        link.addEventListener('click', reply)
+    })
+}
+else {
+    /**
+     * Add events for Reply and Like comments
+     */
+    document.querySelectorAll('.comment .like').forEach(link => {
+        link.addEventListener('click', () => {
+            alert('Connectez-vous pour utiliser cette fonctionnalité.')
+        })
+    })
+    document.querySelectorAll('.comment .reply').forEach(link => {
+        link.addEventListener('click', () => {
+            alert('Connectez-vous pour utiliser cette fonctionnalité.')
+        })
+    })
+}
