ceacf4de6b434f38481ebc348ed078bf613e0020
Couapy <couapy.web@gmail.com>
2020-03-17 11:42:33 +0100

    Add user profile

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('tag/<slug:slug_tag>', views.tag, name='tag'),
+    path('test/<slug:slug>/', views.test, name='test'),
+    path('profile/<int:user_id>', views.profile, name='profile'),
     path('<slug:slug>/', views.post, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 """This file contains all view for the blog application."""
 from django.shortcuts import render
 from django.http import Http404
-from .models import Post, Tag
+from .models import Post, Tag, Profile
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -19,13 +20,38 @@ def tag(request, slug_tag):
         tag = Tag.objects.filter(value=slug_tag)
     except Tag.DoesNotExist:
         raise Http404("Le tag que vous recherchez n'existe pas.")
-    
+
     posts = Post.objects.filter(tags__in=tag)
     context = {
         'posts': posts
     }
 
     return render(request, 'blog/index.html', context)
+
+
+def test(request, slug):
+    """This is the view for posts."""
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        raise Http404("Le post que vous cherchez n'existe pas.")
+    context = {
+        'post': post,
+    }
+    return render(request, 'test.html', context)
+
+
+def profile(request, user_id):
+    """This is the view for user profile."""
+    try:
+        user = User.objects.get(id=user_id)
+        profile = Profile.objects.get(user=user)
+    except Post.DoesNotExist:
+        raise Http404("Le profile que vous cherchez n'existe pas.")
+    context = {
+        'user': user,
+    }
+    return render(request, 'blog/profile.html', context)
 
 
 def post(request, slug):
