84c3202468007d8424edc6fb7b8c91547905c198
Couapy <couapy.web@gmail.com>
2020-03-23 17:46:21 +0100

    UPDATE Post model : convert illustration to ImageField Profile.image is now resized if a side is superior than 256px Post.illustration and Profile.image reset old images

--- Toi/settings.py
+++ Toi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'nho_sawy@5=t@b2kqtfbc%3o+u$bv=hzpv+_1*%jl@xt@am+eq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
